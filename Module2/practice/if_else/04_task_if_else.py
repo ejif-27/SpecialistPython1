2cde2770626168547a8c76c16cdc2f2e0dfd37b2
ejif-27 <57347667+ejif-27@users.noreply.github.com>
2021-06-29 12:47:05 +0300

    Update 04_task_if_else.py

--- Module2/practice/if_else/04_task_if_else.py
+++ Module2/practice/if_else/04_task_if_else.py
@@ -4,4 +4,11 @@
 # а если делится на 3 и на 5 - вывести “Foobar”.
 # Для всех остальных случаев не выводить ничего.
 
-# TODO: your code here
+number = int(input(":"))  
+
+if number % 3 == 0 and number % 5 == 0:
+    print("Foobar")
+elif number % 3 == 0:
+    print("Foo")
+elif number % 5 == 0:
+    print("Bar")
