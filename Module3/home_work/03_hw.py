c723c3bfc9999f1092ff1bbd84b0c0609304ccd5
ejif-27 <57347667+ejif-27@users.noreply.github.com>
2021-06-30 20:37:06 +0300

    Update 03_hw.py

--- Module3/home_work/03_hw.py
+++ Module3/home_work/03_hw.py
@@ -2,3 +2,16 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+
+import random
+numbers = []
+summa = 0
+n = int(input("Введите число элементов: "))
+for i in range(n):
+    numbers.append(random.randint(-100, 100))
+for el in numbers:
+    if el > 0 and el % 2 == 0:
+        summa = summa + el
+
+print(numbers)
+print("=", summa)
