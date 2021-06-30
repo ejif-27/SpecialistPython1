51301f41f7ba4fd0cd843ecc1473448934ff93a2
ejif-27 <57347667+ejif-27@users.noreply.github.com>
2021-06-30 23:23:11 +0300

    Update 06_hw.py

--- Module3/home_work/06_hw.py
+++ Module3/home_work/06_hw.py
@@ -32,14 +32,36 @@ items = [
     },
 ]
 # Найдите:
-print("Товары на складе представлены брэндами: ")
+brands = []
+for item in items:
+    if item["brand"] not in brands:
+        brands.append(item["brand"])
+print("Товары на складе представлены брэндами: ",", ".join(brands))
 
 # TODO: your code here
-
-print("На складе больше всего товаров брэнда(ов): ")
+sum_brands = dict.fromkeys(brands, 0)
+max_sum = 0
+max_brand_list = []
+for item in items:
+    for brand in sum_brands:
+        if item["brand"] == brand:
+            sum_brands[brand] += 1
+for brand in sum_brands:
+    if sum_brands[brand] > max_sum:
+        max_sum = sum_brands[brand]
+for brand in sum_brands:
+    if sum_brands[brand] == max_sum:
+        max_brand_list.append(brand + ' = ' + str(max_sum))
+print("На складе больше всего товаров брэнда(ов): ",", ".join(max_brand_list))
 
 # TODO: your code here
+max_price = 0
+max_price_brand_list = []
+for item in items:
+    if item["price"] > max_price:
+        max_price = item["price"]
+for item in items:
+    if item["price"] == max_price:
+        max_price_brand_list.append(item["brand"])
 
-print("На складе самый дорогой товар брэнда(ов): ")
-
-# TODO: your code here
+print("На складе самый дорогой товар брэнда(ов): ", ", ".join(max_price_brand_list))
