1b1bbe35cf3e290b5c4b4786baf7bbc8abe69959
ejif-27 <57347667+ejif-27@users.noreply.github.com>
2021-06-30 21:51:53 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -1,6 +1,16 @@
 # Дан список имен.
 # Найдите самое длинное имя, если таких имен несколько - выведи любое их них
 
-names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
+import random
+names = ["Иван", "Ирина", "Вячеслав", "Вrчеслав", "Болеслав", "Василий", "Автандил", "Петр" , "Ульманас"]
+max_len_str = 0
+for name in names:
+    if len(name) > max_len_str:
+        max_len_str = len(name)
+        max_name = name
+    elif len(name) == max_len_str:
+        max_name_list = [name, max_name]
+        max_name = random.choice(max_name_list)
+print(max_name)
 
 # TODO: your code here
