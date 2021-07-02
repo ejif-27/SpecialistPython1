f9a013572847acbfeaf64010abd825f876ee96e1
ejif-27 <57347667+ejif-27@users.noreply.github.com>
2021-07-02 11:22:31 +0300

    Update 01_task_repeat.py

--- Module5/practice/repeat/01_task_repeat.py
+++ Module5/practice/repeat/01_task_repeat.py
@@ -2,6 +2,14 @@
 # произвольными целыми числами в заданном диапазоне.
 # , где size - размер генерируемого списка c элементами в диапазоне от of до to.
 
-def gen_list(size, of, to):
-    pass
+import random
 
+
+def gen_list(size, of, to):
+    list_num = []
+
+    for _ in range(size):
+        list_num.append(random.randint(of, to))
+    return list_num
+
+print(gen_list(10, -60, 0))
